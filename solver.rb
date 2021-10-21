5401cffe921283288f4f3c92e44fefd4d3cce911
sneeu-leeu <sa070197@gmail.com>
2021-10-21 15:08:19 +0200

    create factorial method for solver class

--- solver.rb
+++ solver.rb
@@ -1,3 +1,13 @@
 class Solver
-  
+
+  def factorial(num)
+    
+    raise 'Please enter a positive integer' if num.negative?
+
+   if num.zero?
+    1
+   else
+    (1..num).inject(:*) || 1
+   end
+  end
 end
