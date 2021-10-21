c8a428808148e44e5b5c57c67f2e92b3ce5a1886
Caleb Anderson <buyananderson@gmail.com>
2021-10-21 23:39:48 +0200

    Correct remaining linter errors 2

--- solver.rb
+++ solver.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-class Solver # rubocop:todo Style/Documentation
+# class solver
+class Solver
   def factorial(num)
     raise 'Please enter a positive integer' if num.negative?
 
